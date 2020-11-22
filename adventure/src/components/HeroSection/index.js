053426acaa7006172fdb531535833d5254cc2067
klcysn <klcysn@users.noreply.github.com>
2020-11-22 18:08:25 +0100

    Fix services pages responsive bugs and change homepage video source

--- adventure/src/components/HeroSection/index.js
+++ adventure/src/components/HeroSection/index.js
@@ -22,7 +22,9 @@ export function HeroSection(){
     
     return(
         <div className="hero-container">
-            <iframe src="https://www.youtube.com/embed/NzNG_BfCyRA?autoplay=1&mute=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            {!big && <iframe className="home-background" src="https://www.youtube.com/embed/JO0_ZES0zHw?autoplay=1&mute=1&loop=1&playlist=JO0_ZES0zHw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope;" allowfullscreen></iframe>}
+
+            {big && <img className="home-background" src="https://lp-cms-production.imgix.net/2020-07/shutterstockRF_188332622.jpg" />}
             
             {!big && <Carousel />}
             <h1>ADVENTURE</h1>
